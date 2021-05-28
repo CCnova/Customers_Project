f23223c7f2f1e3e2230fa5ea817125531524ab5b
CCnova <caiocnova1@gmail.com>
2021-05-28 17:07:24 -0300

    Add Customer Model, also add the customers app to the installed apps of the project

--- customers/models.py
+++ customers/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+class Customer(models.Model):
+  GENDER_CHOICES = (('M', 'Male'), ('F', 'Female'))
+  first_name = models.CharField(max_length=200)
+  last_name = models.CharField(max_length=200)
+  email = models.EmailField()
+  gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+  company = models.CharField(max_length=200)
+  city = models.CharField(max_length=200)
+  title = models.CharField(max_length=200)
