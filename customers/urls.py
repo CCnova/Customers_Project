4b48eb391d6d72afdfb1ef28b02e432b4c5410b8
CCnova <caiocnova1@gmail.com>
2021-05-30 10:26:18 -0300

    Add templates folder in customers app

--- customers/urls.py
+++ customers/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.index)
+  path('', views.index),
+  path('all_customers', views.all_customers, name='all customers'),
+  path('<int:customer_id>/', views.single_customer, name='single customer'),
 ]
--- customers/views.py
+++ customers/views.py
@@ -1,7 +1,20 @@
+import os
+
 from django.http import HttpResponse
 from django.shortcuts import render
+from myproject.settings import BASE_DIR
+
+from customers.models import Customer
 
 
 # Create your views here.
 def index(request):
-  return HttpResponse('All customers view')
+  return render(request, 'customers/index.html')
+
+def all_customers(request):
+  all_customers = Customer.objects.all()
+  return HttpResponse(all_customers)
+
+def single_customer(request, customer_id):
+  # customer = Customer.objects.(customer_id)
+  return HttpResponse(f'Customer {customer_id} page')
